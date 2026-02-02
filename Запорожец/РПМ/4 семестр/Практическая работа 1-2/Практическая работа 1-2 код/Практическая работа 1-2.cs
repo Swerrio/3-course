d373b1808512c2c20d4c4c672931fb20e514c193
Swerrio <120246505+Swerrio@users.noreply.github.com>
2026-02-02 10:21:33 +0300

    Add files via upload

---
+++ Запорожец/РПМ/4 семестр/Практическая работа 1-2/Практическая работа 1-2 код/Практическая работа 1-2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Назначаем обработчики для каждой кнопки
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+            button4.Click += button4_Click;
+            button5.Click += button5_Click;
+
+            // Обработчик изменения размера формы
+            this.Resize += Form1_Resize;
+
+            // Изначально делаем все кнопки зелёными
+            button1.BackColor = Color.Green;
+            button2.BackColor = Color.Green;
+            button3.BackColor = Color.Green;
+            button4.BackColor = Color.Green;
+            button5.BackColor = Color.Green;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Red;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.Red;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.Red;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.Red;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // При изменении размера все кнопки делают активными (зелёными)
+            button1.BackColor = Color.Green;
+            button2.BackColor = Color.Green;
+            button3.BackColor = Color.Green;
+            button4.BackColor = Color.Green;
+            button5.BackColor = Color.Green;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Перекрашиваем все кнопки в зелёный
+            button1.BackColor = Color.Green;
+            button2.BackColor = Color.Green;
+            button3.BackColor = Color.Green;
+            button4.BackColor = Color.Green;
+            button5.BackColor = Color.Green;
+
+            // Увеличиваем размер формы на 10 пикселей в ширину и высоту
+            this.Width -= 10;
+            this.Height -= 10;
+        }
+
+    }
+}
