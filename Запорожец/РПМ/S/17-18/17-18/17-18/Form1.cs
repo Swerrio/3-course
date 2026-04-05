ee3e78a666947aa5067571dd324bd0b24854486e
Swerrio <120246505+Swerrio@users.noreply.github.com>
2026-04-06 02:00:30 +0300

    Add files via upload

---
+++ Запорожец/РПМ/S/17-18/17-18/17-18/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17_18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.SkyBlue);
+
+            // солнце
+            SolidBrush sunBrush = new SolidBrush(Color.Yellow);
+            g.FillEllipse(sunBrush, 50, 50, 80, 80);
+
+            // Лучи солнца
+            Pen rayPen = new Pen(Color.Orange, 3);
+            rayPen.DashStyle = DashStyle.Dash;
+            g.DrawLine(rayPen, 90, 40, 90, 10);
+
+            rayPen.DashStyle = DashStyle.DashDot;
+            g.DrawLine(rayPen, 140, 90, 180, 90);
+
+            // Трава
+            SolidBrush grassBrush = new SolidBrush(Color.ForestGreen);
+            g.FillRectangle(grassBrush, 0, 300, this.Width, this.Height - 300);
+
+            // Дом
+            SolidBrush houseBrush = new SolidBrush(Color.BurlyWood);
+            g.FillRectangle(houseBrush, 200, 200, 150, 150);
+            g.DrawRectangle(Pens.Black, 200, 200, 150, 150);
+
+            // Крыша
+            Point[] roofPoints = {
+                new Point(200, 200),
+                new Point(350, 200),
+                new Point(275, 120)
+            };
+            g.FillPolygon(Brushes.Brown, roofPoints);
+            g.DrawPolygon(Pens.Black, roofPoints);
+
+            // Забор
+            Pen fencePen = new Pen(Color.SaddleBrown, 4);
+            for (int x = 10; x < 180; x += 20)
+            {
+                g.DrawLine(fencePen, x, 320, x, 280);
+            }
+            g.DrawLine(fencePen, 5, 300, 185, 300);
+
+            // Облако
+            g.FillEllipse(Brushes.White, 400, 60, 100, 50);
+            g.FillEllipse(Brushes.White, 430, 40, 80, 60);
+        }
+    }
+}
---
+++ Запорожец/РПМ/S/17-18/19/19/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19
+{
+    public partial class Form1 : Form
+    {
+        // Угол
+        private double angleEarth = 0;
+        private double angleMoon = 0;
+
+        // Радиусы орбит
+        private const int sunOrbitRadius = 150;
+        private const int earthOrbitRadius = 40;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; // Важно для плавной анимации
+            this.BackColor = Color.Black; // Космос
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int cx = ClientSize.Width / 2;
+            int cy = ClientSize.Height / 2;
+
+            // Рисуем Солнце
+            g.FillEllipse(Brushes.Yellow, cx - 30, cy - 30, 60, 60);
+
+            // Рассчитываем координаты Земли
+            int earthX = cx + (int)(sunOrbitRadius * Math.Cos(angleEarth));
+            int earthY = cy + (int)(sunOrbitRadius * Math.Sin(angleEarth));
+
+            // Рисуем орбиту Земли (пунктиром)
+            Pen orbitPen = new Pen(Color.DimGray, 1);
+            orbitPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            g.DrawEllipse(orbitPen, cx - sunOrbitRadius, cy - sunOrbitRadius, sunOrbitRadius * 2, sunOrbitRadius * 2);
+
+            // Рисуем Землю
+            g.FillEllipse(Brushes.DeepSkyBlue, earthX - 12, earthY - 12, 24, 24);
+
+            // Рассчитываем координаты Луны (относительно Земли!)
+            int moonX = earthX + (int)(earthOrbitRadius * Math.Cos(angleMoon));
+            int moonY = earthY + (int)(earthOrbitRadius * Math.Sin(angleMoon));
+
+            // Рисуем Луну
+            g.FillEllipse(Brushes.LightGray, moonX - 5, moonY - 5, 10, 10);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Увеличиваем углы
+            angleEarth += 0.02;
+            angleMoon += 0.08;
+
+            // Перерисовываем форму
+            Invalidate();
+        }
+    }
+}
